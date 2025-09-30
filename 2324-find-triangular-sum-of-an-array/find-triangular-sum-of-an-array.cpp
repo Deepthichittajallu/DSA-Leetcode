3c72bc6bb99706373cf70b8f9c1bb3306eee8302
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2025-09-30 20:01:37 +0530

    Time: 233 ms (25.02%) | Memory: 271 MB (23.93%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -2,14 +2,13 @@
 // public:
 //     int triangularSum(vector<int>& nums) {
 //         vector<vector<int>>res;
-//         res.push_back(nums);
 //         int n = nums.size();
-//         for(int i=0;i<n-1;i++)
+//         for(int i=0;i<n;i++)
 //         {
-//             vector<int>row(0,n-1-i);
-//             for(int j=0;j<n-i-1;j++)
+//             vector<int>row(n-i,0);
+//             for(int j=0;j<n-i;j++)
 //             {
-//                 row[j] = (res[i-1][j]+res[i-1][j+1])%10;
+//                 row[j] = 0;
 //             }
 //             res.push_back(row);
 //         }
@@ -17,50 +16,36 @@
 //         {
 //             for(int j=0;j<res[i].size();j++)
 //             {
-//                 cout<<res[i][j]<<" ";
+//                 if(i==0)
+//                 {
+//                     res[i][j] = nums[j];
+//                 }
+//                 else
+//                 {
+//                     res[i][j] = (res[i-1][j] + res[i-1][j+1])%10;
+//                 }
 //             }
-//             cout<<endl;
 //         }
-//         return 0;
+//         int ans = res[n-1].size();
+//         return res[n-1][ans-1];
 //     }
 // };
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
         vector<vector<int>>res;
+        res.push_back(nums);
         int n = nums.size();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            vector<int>row(n-i,0);
-            for(int j=0;j<n-i;j++)
+            vector<int>row(n-1-i,0);
+            for(int j=0;j<n-i-1;j++)
             {
-                row[j] = 0;
+                row[j] = (res[i][j]+res[i][j+1])%10;
             }
             res.push_back(row);
         }
-        for(int i=0;i<res.size();i++)
-        {
-            for(int j=0;j<res[i].size();j++)
-            {
-                if(i==0)
-                {
-                    res[i][j] = nums[j];
-                }
-                else
-                {
-                    res[i][j] = (res[i-1][j] + res[i-1][j+1])%10;
-                }
-            }
-        }
-        // for(int i=0;i<res.size();i++)
-        // {
-        //     for(int j=0;j<res[i].size();j++)
-        //     {
-        //         cout<<res[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        int ans = res[n-1].size();
-        return res[n-1][ans-1];
+        int m = res[n-1].size();
+        return res[n-1][m-1];
     }
 };
