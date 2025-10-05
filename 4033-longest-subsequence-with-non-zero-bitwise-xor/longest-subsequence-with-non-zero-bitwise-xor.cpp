9ebf5479f8e6d74c864bf78fe5d945cd0a9e4a23
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2025-10-05 12:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 171.1 MB (0.00%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
-        int cnt = 1;
-        int xo = nums[0];
-        int x = 0;
-        map<int,int>mp;
-        if(nums.size() == 1 && nums[0] == 0) return 0;
+        int xo = 0;
+        bool found = false;
         for(int i=0;i<nums.size();i++)
         {
-            mp[nums[i]]++;
-            x = (x^nums[i]);
-        }
-        // for(auto it:mp)
-        // {
-        //     cout<<it.first;
-        // }
-        if(mp.size() == 1 && nums[0] > 0)
-        {
-            if(nums.size()%2 == 0) return nums.size()-1;
-            else return nums.size();
-        }
-        else if(mp.size() == 1 && nums[0] == 0) return 0;
-        else if(x!=0)
-        {
-            return nums.size();
-        }
-        else{
-        for(int i=1;i<nums.size();i++)
-        {
-            if((nums[i]^xo)!=0) 
+            xo = (xo^nums[i]);
+            if(nums[i] != 0)
             {
-                cnt++;
-                xo = (xo^nums[i]);
-            }
-            else
-            {
-                continue;
+                found = true;
             }
         }
-        }
-        return cnt;
+        if(xo != 0) return nums.size();
+        return found ? nums.size()-1 : 0;
     }
 };
