f9192e29bf2b1d21653b723d3137c3b0f342252f
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2025-11-13 22:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 31.6 MB (56.49%) - LeetSync

---
+++ 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int ans = 0;
+        int maxi = *max_element(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            ans += abs(maxi-nums[i]);
+        }
+        return ans;
+    }
+};
