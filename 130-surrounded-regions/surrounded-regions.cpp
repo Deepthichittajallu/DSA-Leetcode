d5c24849a914b56b479e7aea625eb520e5266e7a
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2025-12-19 15:09:25 +0530

    Time: 11 ms (5.97%) | Memory: 14.5 MB (56.82%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        queue<pair<int,int>>q;
+        int m = board.size();
+        int n = board[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(i == 0 || i == m-1 || j == 0 || j == n - 1)
+                {
+                    if(board[i][j] == 'O')
+                    {
+                        board[i][j] = 'P';
+                        q.push({i,j});
+                    }
+                }
+            }
+        }
+        vector<int>dr = {-1,1,0,0};
+        vector<int>dc = {0,0,-1,1};
+        while(!q.empty())
+        {
+            auto top = q.front();
+            cout<<top.first<<" "<<top.second<<endl;
+            int r = top.first;
+            int c = top.second;
+            q.pop();
+            for(int i = 0;i<4;i++)
+            {
+                int row = r + dr[i];
+                int col = c + dc[i];
+                if(row >= 0 && row < m && col >= 0 && col < n)
+                {
+                    if(board[row][col] == 'O')
+                    {
+                        q.push({row,col});
+                        board[row][col] = 'P';
+                    }
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                if(board[i][j] == 'P') board[i][j] =  'O';
+            }
+        }
+    }
+};
