499a515811152914bd7004103877a614c25edae2
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2025-12-23 22:36:51 +0530

    Time: 367 ms (5.02%) | Memory: 163 MB (60.23%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        sort(events.begin(),events.end(),[](const vector<int>&a,const vector<int>&b){
+            return a[1]<b[1];
+        });
+        vector<int>end;
+
+        int n = events.size();
+        vector<int>pre(n,-1);
+        for(int i=0;i<events.size();i++)
+        {
+            end.push_back(events[i][1]);
+        }
+        pre[0] = events[0][2];
+        for(int i=1;i<events.size();i++)
+        {
+            pre[i] = max(events[i][2],pre[i-1]);
+        }
+        int ans = INT_MIN;
+        for(int i=0;i<events.size();i++)
+        {
+            int st = events[i][0];
+            int ind = lower_bound(end.begin(),end.end(),st) - end.begin();
+            // cout<<ind<<" ";
+            if(ind == 0) continue;
+            else {
+                ans = max(pre[ind-1]+events[i][2],ans);
+                cout<<ans<<" ";
+            }
+        }
+        int maxi = *max_element(pre.begin(),pre.end());
+        return max(maxi,ans);
+    }
+};
