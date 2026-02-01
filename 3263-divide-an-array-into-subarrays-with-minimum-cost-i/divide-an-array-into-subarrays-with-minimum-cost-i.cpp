1479d2faefe87a08933de3de91ec68dc8026a83f
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-02-01 19:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (74.87%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -3,10 +3,6 @@ public:
     int minimumCost(vector<int>& nums) {
         int  a = nums[0];
        sort(nums.begin() + 1,nums.end());
-       for(auto it:nums)
-       {
-        cout<<it<<" ";
-       }
        return nums[0] + nums[1] + nums[2]; 
     }
 };
