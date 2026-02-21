08f36197977469c0012f12d65fb1a17dc9d0093b
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-02-21 20:11:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (85.81%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end(),greater<int>());
+        int i=0;
+        while(i<citations.size() && citations[i] > i)
+        {
+            i++;
+        }
+        return i;
+    }
+};
