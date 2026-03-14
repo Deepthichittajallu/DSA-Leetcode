b4bfa91e2bfbb50b9fe1ac821cec680ad1773465
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-03-14 13:31:07 +0530

    Time: 44 ms (9.28%) | Memory: 102.7 MB (5.75%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        vector<pair<pair<int,int>,int>>vec;
+        int n = rocks.size();
+        for(int i=0;i<n;i++)
+        {
+            int dif = capacity[i] - rocks[i];
+            vec.push_back({{dif,capacity[i]},rocks[i]});
+        }
+        int cnt = 0;
+        sort(vec.begin(),vec.end());
+        for(auto it:vec)
+        {
+            int re = it.first.first;
+            if(re <= 0)
+            {   
+                cnt++;
+                 continue;
+            }
+            else if(re > additionalRocks) return cnt;
+            else if(re <= additionalRocks)
+            {
+                cnt++;
+                additionalRocks -= re;
+            }
+        }
+        return cnt;
+    }
+};
