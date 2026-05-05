21078641e29d370303f0eb33b3d248a545639804
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-05-05 21:37:12 +0530

    Time: 62 ms (82.03%) | Memory: 148.5 MB (61.79%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,75 @@
+class MedianFinder {
+public:
+    priority_queue<int>pq1;
+    priority_queue<int,vector<int>,greater<int>>pq2; 
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(pq1.empty()) pq1.push(num);
+        else if(num > pq1.top())
+        {
+            pq2.push(num);
+        }
+        else if(num <= pq1.top())
+        {
+            pq1.push(num);
+        }
+        if(abs((int)pq1.size() - (int)pq2.size()) > 1)
+        {
+            if(pq1.size() > pq2.size())
+            {
+                pq2.push(pq1.top());
+                pq1.pop();
+            }
+            else{
+                pq1.push(pq2.top());
+                pq2.pop();
+            }
+        }
+            // if(pq1.top() < num && abs((int)pq1.size() - (int)pq2.size()) <= 1)
+            // {
+            //     pq2.push(num);
+            // }
+            // else if(pq1.top() >= num  && abs((int)pq1.size() - (int)pq2.size()) <= 1)
+            // {
+            //     pq1.push(num);
+            // }
+            // else{
+            //     if(abs((int)pq1.size() - (int)pq2.size())  > 1)
+            //     {
+            //         if(pq1.size() > pq2.size())
+            //         {
+            //             pq2.push(pq1.top());
+            //             pq1.pop();
+            //         }
+            //         else{
+            //             pq1.push(pq2.top());
+            //             pq2.pop();
+            //         }
+            //     }
+            // }
+    }
+    
+    double findMedian() {
+        if((pq1.size() == pq2.size()) )// % 2 == 0)
+        {
+            return (double)(pq1.top() + pq2.top())/2.0;
+        }
+        else if(pq1.size() > pq2.size())
+        {
+            return (double)pq1.top();
+        }
+        else{
+            return (double)pq2.top();
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
