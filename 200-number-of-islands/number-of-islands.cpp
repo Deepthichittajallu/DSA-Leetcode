a1d108be39a6dce57b6410a7c09aefef4fc31251
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-05-19 14:45:08 +0530

    Time: 29 ms (41.78%) | Memory: 23.9 MB (20.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,59 +1,41 @@
 class Solution {
 public:
-void BFS(vector<vector<char>> &grid,int row,int col)
+void bfs(vector<vector<char>>& grid,int row,int col,int n,int m)
 {
-    int m = grid.size();
-    int n = grid[0].size();
-    // grid[row][col] = 'V';
-    vector<int>dr = {-1,1,0,0};
-    vector<int>dc = {0,0,-1,1};
     queue<pair<int,int>>q;
     q.push({row,col});
+    grid[row][col] = '0';
+    vector<int>dr = {-1,1,0,0};
+    vector<int>dc = {0,0,1,-1};
     while(!q.empty())
     {
-        auto top = q.front();
+        auto front = q.front();
         q.pop();
-        int i = top.first;
-        int j = top.second;
-        grid[i][j] = 'V';
-        for(int k=0;k<4;k++)
+        for(int i=0;i<4;i++)
         {
-            int r = i + dr[k];
-            int c = j + dc[k];
-            // cout<<r<<" "<<c<<endl;
-            if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1')
+            int r = dr[i] + front.first;
+            int c = dc[i] + front.second;
+            if(r >= 0&& r < n && c >= 0&& c < m && grid[r][c] == '1')
             {
-                grid[r][c] = 'V';
+                grid[r][c] = 0;
                 q.push({r,c});
             }
         }
-
     }
+
 }
-// void print(vector<vector<char>>& grid)
-// {
-//     for(int i=0;i<grid.size();i++)
-//     {
-//         for(int j=0;j<grid[i].size();j++)
-//         {
-//             cout<<grid[i][j]<<" ";
-//         }
-//         cout<<endl;
-//     }
-// }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
         int cnt = 0;
-        for(int i=0;i<m;i++)
+        int n = grid.size();
+        int m = grid[0].size();
+        // vector<vector<bool>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
         {
-            for(int j= 0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j] == '1')
                 {
-                    BFS(grid,i,j);
-                    // cout<<endl;
-                    // print(grid);
+                    bfs(grid,i,j,n,m);
                     cnt++;
                 }
             }
