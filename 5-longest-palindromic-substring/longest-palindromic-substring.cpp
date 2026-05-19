489b45a9e8d3c9d05ce46e8bac3ce5bfdb807767
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-05-19 11:05:52 +0530

    Time: 13 ms (57.87%) | Memory: 18.6 MB (46.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,45 +2,44 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<bool>>dp(n,vector<bool>(n,0));
-        for(int i=0;i<n;i++)
+        if(n <= 1) return s;
+        string ans = "";
+        for(int i = 1;i<n;i++)
         {
-            dp[i][i] = 1;
-        }
-        int maxi = 1;
-        int a = 0,b=0;
-        for(int i=0;i<n-1;i++)
-        {
-            if(s[i] == s[i+1])
+            int low = i;
+            int high = i;
+            while(s[low] == s[high])
             {
-                dp[i][i+1] = 1;
-                maxi = 2;
-                a = i;
-                b = i+1;
-            }
-        }
-        for(int len = 3;len<=n;len++)
-        {
-            for(int i=0;i<=(n-len);i++)
-            {
-                int j = i + (len - 1);
-                if(s[i] == s[j] && dp[i+1][j-1] == 1)
+                low--;
+                high++;
+                if(low == -1 || high == n)
                 {
-                    dp[i][j] = 1;
-                   if(len > maxi)
-                   {
-                     a = i;
-                     b = j;
-                     maxi = len;
-                   }
+                    break;
                 }
             }
+            string temp = s.substr(low+1,high - low - 1);
+            if(temp.size() > ans.size())
+            {
+                ans = temp;
+            }
+            low = i-1;
+            high = i;
+            while(s[low] == s[high])
+            {
+                low--;
+                high++;
+                if(low == -1 || high == n)
+                {
+                    break;
+                }
+            }
+            temp = s.substr(low+1,high - low - 1);
+            if(temp.size() > ans.size())
+            {
+                ans = temp;
+            }
+
         }
-        string res = "";
-      for(int i=a;i<=b;i++)
-      {
-        res += s[i];
-      }
-        return  res;
+        return ans;
     }
 };
