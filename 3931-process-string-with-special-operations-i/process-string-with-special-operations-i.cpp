da1bc6fa46d58d9f4101cbb66db3c33653e8ef68
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-06-17 10:38:58 +0530

    Time: 3 ms (76.81%) | Memory: 47 MB (10.65%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string processStr(string s) {
+       string res = "" ;
+       for(int i=0;i<s.size();i++)
+       {
+            if(s[i] == '*')
+            {
+                if(!res.empty()) res.pop_back();
+            } 
+            else if(s[i] == '#')
+            {
+                string temp = res;
+                res += temp;
+            }
+            else if(s[i] == '%')
+            {
+                reverse(res.begin(),res.end());
+            }
+            else
+            {
+                res += s[i];
+            }
+       }
+       return res;
+    }
+};
