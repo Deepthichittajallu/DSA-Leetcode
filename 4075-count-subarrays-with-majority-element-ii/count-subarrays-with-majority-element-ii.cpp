7ffdc914c8ebcb1568c9bd53fe2b937968f4736f
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-06-26 12:03:11 +0530

    Time: 580 ms (19.20%) | Memory: 183.1 MB (33.11%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -1,26 +1,25 @@
-
-// #include<ext/pb_ds/tree_policy.hpp>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 class Solution {
 public:
-    typedef tree<pair<int,int>,null_type,less<pair<int ,int>>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+typedef tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,
+tree_order_statistics_node_update>pbds;
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         pbds os;
-        long long ans = 0;
+        os.insert({0,-1});
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i] == target) nums[i] = 1;
             else nums[i] = -1;
         }
-        int temp = 0;
-        os.insert({0,-1});
+        int pre = 0;
+        long long ans = 0;
         for(int i=0;i<nums.size();i++)
         {
-            temp += nums[i];
-            ans += os.order_of_key({temp,-1});
-            os.insert({temp,i});
+            pre += nums[i];
+            ans += os.order_of_key({pre,-1});
+            os.insert({pre,i});
         }
         return ans;
     }
