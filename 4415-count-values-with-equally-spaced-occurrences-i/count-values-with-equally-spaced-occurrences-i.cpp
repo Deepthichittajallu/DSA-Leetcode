807e2ad9e9481a27abd09e45f58153deda364020
Lakshmi Deepthi Chittajallu <150007663+Deepthichittajallu@users.noreply.github.com>
2026-09-13 18:30:52 +0530

    Time: 15 ms (20.00%) | Memory: 35.3 MB (10.00%) - LeetSync

---
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        map<int,vector<int>>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]].push_back(i);
+        }
+        int cnt = 0;
+        for(auto it:mp)
+        {
+            if(it.second.size() < 3) continue;
+            else if(it.second.size() == 3)
+            {
+                map<int,int>temp;
+                auto vec = it.second;
+                for(int i=1;i<vec.size();i++)
+                {
+                    temp[vec[i]-vec[i-1]]++;
+                }
+                if(temp.size() == 1) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
